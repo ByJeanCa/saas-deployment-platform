3f0204c0b69607d4113dda7280406a6a6f90417d
byjeanca <jo7624822@gmail.com>
2026-03-27 14:11:07 -0600

    feat(infra): add ECR repositories module for application images

---
+++ infra-boostrap/modules/ecr/main.tf
@@ -0,0 +1,13 @@
+
+resource "aws_ecr_repository" "images" {
+
+  for_each = toset(var.image_names)
+  name                 = format("repo-%s-image", each.value)
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = var.common_tags
+}
