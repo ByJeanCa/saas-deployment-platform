780a0b76031c7e147effa04030556728e47d69cd
byjeanca <jo7624822@gmail.com>
2026-04-03 17:13:03 -0600

    feat: Add private_subnet_ids output

--- infra-boostrap/modules/network/outputs.tf
+++ infra-boostrap/modules/network/outputs.tf
@@ -1,11 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the main VPC."
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  value = [for k in sort(keys(aws_subnet.public)) : aws_subnet.public[k].id]
+  description = "The IDs of the public subnets."
+  value       = [for k in sort(keys(aws_subnet.public)) : aws_subnet.public[k].id]
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets."
+  value       = [for k in sort(keys(aws_subnet.private)) : aws_subnet.private[k].id]
 }
 
 output "db_group_name" {
-  value = aws_db_subnet_group.db.name
+  description = "The name of the DB subnet group."
+  value       = aws_db_subnet_group.db.name
 }
